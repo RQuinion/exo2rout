4654d62795c38eb09181fc2ef277c8ed434634d5
romain <r.quinion@gmail.com>
2023-02-13 16:07:35 +0100

    exo 2 fait

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card';
+import React from 'react';
 
-function App() {
+const MonComposant = (props) => {
+  const { title, description } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+      <Card descri={"description de l'image"} name={"image"} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function card(props) {
+    const {card} = props;
+  return (
+    <div>
+        
+    </div>
+  )
+}
